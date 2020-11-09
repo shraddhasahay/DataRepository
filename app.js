99406c981998106ed7a34d431af3ed37ad295dc9
Lone-Star5 <amankap05@gmail.com>
2020-11-09 22:18:48 +0530

    added login page but needs further backend support with database

--- app.js
+++ app.js
@@ -1,10 +1,22 @@
 const express = require('express')
 const app = express();
 
+// Setting public direction
+app.use(express.static(__dirname + "/public"));
+// Set ejs template
+app.set("view engine", "ejs");
+
+// Routes
 app.get('/', (req,res)=>{
     res.send('Faculty Module');
 })
 
+// Login Page
+app.get('/login', (req,res)=>{
+    res.render('login');
+})
+
+// Server Running at port 3000
 app.listen('3000', ()=>{
     console.log('Server Started ...')
 })
