d94b617ce69bc94b6199afb798e38506a83cea5d
Arpan Abhishek <arpanforbusiness@gmail.com>
2020-11-11 15:35:21 +0530

    complete login and registration flow

--- app.js
+++ app.js
@@ -1,26 +1,103 @@
-const express = require('express')
+const express = require("express");
 const app = express();
+
+var mysql = require("mysql");
+var connection = mysql.createConnection({
+  host: "localhost",
+  user: "root",
+  password: "",
+  database: "faculty_module",
+});
+connection.connect(function (err) {
+  if (!err) {
+    console.log("Database is connected ...");
+  } else {
+    console.log("Error connecting database ...");
+  }
+});
 
 // Setting public direction
 app.use(express.static(__dirname + "/public"));
 // Set ejs template
 app.set("view engine", "ejs");
 
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
 // Routes
-app.get('/', (req,res)=>{
-    res.send('Faculty Module');
-})
+app.get("/", (req, res) => {
+  res.send("Faculty Module");
+});
 
 // Login Page
-app.get('/login', (req,res)=>{
-    res.render('login');
-})
+app.get("/login", (req, res) => {
+  res.render("login");
+});
 
-app.get('/register', (req,res)=>{
-    res.render('register');
-})
+app.post("/login", (req, res) => {
+  const { email, password } = req.body;
+  console.log(email, password);
+
+  connection.query("SELECT * FROM Faculty WHERE mailId = ?", [email], function (
+    error,
+    results,
+    fields
+  ) {
+    if (error) {
+      res.send({
+        code: 400,
+        failed: "error ocurred",
+      });
+    } else {
+      if (results.length > 0) {
+        const comparision = password === results[0].password;
+        if (comparision) {
+          res.send({
+            code: 200,
+            success: "login sucessfull",
+          });
+        } else {
+          res.send({
+            code: 204,
+            success: "Email and password does not match",
+          });
+        }
+      } else {
+        res.send({
+          code: 206,
+          success: "Email does not exits",
+        });
+      }
+    }
+  });
+});
+
+app.get("/register", (req, res) => {
+  res.render("register");
+});
+
+app.post("/register", (req, res) => {
+  connection.query("INSERT INTO Faculty SET ?", req.body, function (
+    error,
+    results,
+    fields
+  ) {
+    if (error) {
+      res.send({
+        code: 400,
+        failed: "error ocurred",
+      });
+    } else {
+      //   res.send({
+      //     code: 200,
+      //     success: "user registered sucessfully",
+      //   });
+      res.redirect("/login");
+    }
+  });
+});
 
 // Server Running at port 3000
-app.listen('3000', ()=>{
-    console.log('Server Started ...')
-})
+app.listen("3000", () => {
+  console.log("Server Started ... http://localhost:3000");
+});
