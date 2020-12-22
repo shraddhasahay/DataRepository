e1fcaf3468fdb21dfccbc87f2774e370708c776c
dreadwing5 <sachinspyk@outlook.com>
2020-12-23 00:03:44 +0530

    Adding user page

--- routes/admin.js
+++ routes/admin.js
@@ -4,7 +4,21 @@ const router = express.Router();
 
 //Admin Panel
 router.get('/admin', function (req, res){
-    res.render('admin_panel');
+    res.render('admin/admin_panel',{
+        title : 'Dashboard'
+    });
+});
+
+//Add User Page
+router.get('/add-user', function (req, res){
+    res.render('admin/admin_addUser',{
+        title : 'Add User'
+    });
+});
+
+//Login Page
+router.get('/login',function(req,res){
+    res.render('login');
 });
 
 module.exports = router;
